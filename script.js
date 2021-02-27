c698e8108b11df91698d05491d7e195f96abea7a
ngd929 <59147297+ngd929@users.noreply.github.com>
2021-02-26 23:45:10 -0600

    working version

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // Get input values
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+
+    // Ensure principal amount is a positive number
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    let interest = principal * years * rate / 100;
+    let yearFromNow = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal.toString() + 
+    "</mark>,<br>at an interest rate of <mark>" + rate.toString() +
+    "</mark>%.<br>You will receive an amount of <mark>" + interest.toString() +
+    "</mark>,<br>in the year <mark>" + yearFromNow.toString()+"</mark>";    
+}
+
+function detectChange(value) {
+    document.getElementById("output").value = value + ' %';
 }
         
